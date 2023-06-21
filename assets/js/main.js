207582eea54ecd393b4367c0c2d903fba6b60700
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-06-21 23:36:54 +0530

    Done Now Only Updation Left

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,13 +125,42 @@ let swiper = new Swiper('.portfolio__container', {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
 const themeButton = document.getElementById('theme-button')
@@ -163,3 +192,25 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
+/*==================== Filters Certificate & Skill ====================*/
+
+const tabss = document.querySelectorAll('[data-targets]');
+const tabscontent = document.querySelectorAll('[data-contents]');
+
+tabss.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.targets);
+
+    tabscontent.forEach(tc => {
+      tc.classList.remove('filters__active');
+    });
+
+    target.classList.add('filters__active');
+
+    tabss.forEach(t => {
+      t.classList.remove('filter-tab-active');
+    });
+
+    tab.classList.add('filter-tab-active');
+  });
+});
